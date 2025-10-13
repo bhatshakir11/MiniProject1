0ab90f536ba15ca37ab8aca4507577f954621b4c
bhatshakir11 <b.shakir8491@gmail.com>
2025-10-13 12:39:33 +0530

    added download pdf option

--- cyberhygine-backend/main.py
+++ cyberhygine-backend/main.py
@@ -266,7 +266,7 @@ def generate_pdf_report():
             ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
             ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            ('BOTTOMPADDING', (0, 0), (-1, .0), 12),
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
             ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
             ('GRID', (0, 0), (-1, -1), 1, colors.black)
         ]))
